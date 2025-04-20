18835e9f1b6e52b27b577938b5c5c5e1a89bf18a
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2025-04-20 22:20:42 +0300

    Update CORS configuration and modify API endpoint URLs in test cases

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://10.120.32.74', 'https://users.metropolia.fi'],
+    origin: [
+      'http://10.120.32.74',
+      'https://users.metropolia.fi',
+      'http://127.0.0.1:5500',
+    ],
     credentials: true,
   })
 );
